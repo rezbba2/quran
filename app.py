fdd02a401bd7d9dda6f75702d9feb9e3c0bc1d8a
rezarogh <91724979+rezarogh@users.noreply.github.com>
2023-03-23 17:35:11 +0430

    set port

--- app.py
+++ app.py
@@ -105,5 +105,7 @@ def import_data():
 
 
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+      app.run(debug=True, host='0.0.0.0', port=8000)
